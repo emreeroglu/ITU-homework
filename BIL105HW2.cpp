a5e4304c55215058a6c34a4d79c37da5bd73da76
EmiXLabs <emin@mastizadeh.co.cc>
2013-03-27 02:44:19 +0200

    homework #2

--- BIL105HW2.cpp
+++ BIL105HW2.cpp
@@ -22,7 +22,12 @@ int main()
 	while (checker == false)
 	{
 		cin >> N >> M;
+<<<<<<< HEAD
+		if (cin.fail()) { cout << "\nERROR!!!\n\nInvalid symbol for size. Enter number!\n\n"; return 1; }
+		if (N > 0 && M > 0 )
+=======
 		if (N > 0 && M > 0 && !cin.fail() )
+>>>>>>> afdfd6fbb2a4e39dde8abc674f8c64d8b4a8ac6e
 		{
 			if (N % 2 != 0)
 			{ checker = true; }
@@ -34,11 +39,29 @@ int main()
 	while (checker == false)
 	{
 		cin >> alc;
+<<<<<<< HEAD
+		if (cin.fail()) { cout << "\nERROR!!!\n\nInvalid symbol for alcohol level. Enter number!\n\n"; return 1; }
+		if (alc >= 0 && alc <= 1)
+		{ checker = true; }
+		else { cout << "\nPlease, enter number between 0 and 1:\t"; }
+	} checker = false;
+	
+	cout << "\nNumber of tries/simulations (S):\t";
+		while (checker == false)
+	{
+		cin >> tries;
+		if (cin.fail()) { cout << "\nERROR!!!\n\nInvalid symbol for tries. Enter number!\n\n"; return 1; }
+		if (tries > 0)
+		{ checker = true; }
+		else { cout << "\nPlease, enter natural number for tries:\t"; }
+	} checker = false;
+=======
 		checker = true;
 	}
 	
 	cout << "\nNumber of tries/simulations (S):\t";
 	cin >> tries;
+>>>>>>> afdfd6fbb2a4e39dde8abc674f8c64d8b4a8ac6e
 	
 	return 0;
 }
