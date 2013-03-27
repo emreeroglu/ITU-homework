a18f6c37d629a9c2e4f3f8e71ea9e062f84c90c9
EmiXLabs <emin@mastizadeh.co.cc>
2013-03-27 02:49:05 +0200

    homework #2

--- BIL105HW2.cpp
+++ BIL105HW2.cpp
@@ -22,12 +22,8 @@ int main()
 	while (checker == false)
 	{
 		cin >> N >> M;
-<<<<<<< HEAD
 		if (cin.fail()) { cout << "\nERROR!!!\n\nInvalid symbol for size. Enter number!\n\n"; return 1; }
 		if (N > 0 && M > 0 )
-=======
-		if (N > 0 && M > 0 && !cin.fail() )
->>>>>>> afdfd6fbb2a4e39dde8abc674f8c64d8b4a8ac6e
 		{
 			if (N % 2 != 0)
 			{ checker = true; }
@@ -39,7 +35,6 @@ int main()
 	while (checker == false)
 	{
 		cin >> alc;
-<<<<<<< HEAD
 		if (cin.fail()) { cout << "\nERROR!!!\n\nInvalid symbol for alcohol level. Enter number!\n\n"; return 1; }
 		if (alc >= 0 && alc <= 1)
 		{ checker = true; }
@@ -55,13 +50,6 @@ int main()
 		{ checker = true; }
 		else { cout << "\nPlease, enter natural number for tries:\t"; }
 	} checker = false;
-=======
-		checker = true;
-	}
-	
-	cout << "\nNumber of tries/simulations (S):\t";
-	cin >> tries;
->>>>>>> afdfd6fbb2a4e39dde8abc674f8c64d8b4a8ac6e
 	
 	return 0;
 }
